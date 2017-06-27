01ad77416faec69b7a87dd0144e215c0feeba99a
Evan Kiser <ekiser247@gmail.com>
2017-06-27 17:52:48 -0500

    added a player controller

---
+++ Assets/PlayerController.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+    public float speed = 10;
+    public float jumpForce = 1;
+    public int maxJumpCount = 1;
+    public int jumpCount = 1;
+    bool onGround = true;
+    private Rigidbody2D body;
+    private Animator anim;
+
+	void Start ()
+    {
+        body = GetComponent<Rigidbody2D>();
+        anim = GetComponent<Animator>();	
+	}
+	
+	void Update ()
+    {
+        float x = Input.GetAxis("Horizontal");
+        body.velocity = new Vector2((x * speed), body.velocity.y);
+
+        JumpRoutine();
+        Grounded();	
+	}
+
+    void JumpRoutine()
+    {
+        if (Input.GetButtonDown("Jump") && onGround == true)
+        {
+            body.AddForce(Vector2.up * jumpForce, ForceMode2D.Impulse);
+            --jumpCount;
+            if(jumpCount == 0)
+            {
+                onGround = false;
+            }
+        }
+    }
+
+    void Grounded()
+    {
+        if (body.velocity.y == 0)
+        {
+            onGround = true;
+            jumpCount = maxJumpCount;
+        }
+    }
+}
--- Assets/TileMap/Editor/TileMapEditor.cs
+++ Assets/TileMap/Editor/TileMapEditor.cs
@@ -6,129 +6,149 @@ using System;
 using ClipperLib;
 
 [CustomEditor(typeof(TileMap))]
-public class TileMapEditor : Editor {
+public class TileMapEditor : Editor
+{
 
-	public static int tile = 0;
-	
-	public TileMap tileMap {
-		get { return target as TileMap; }
-	}
+    public static int tile = 0;
 
-	void OnEnable () {
-		Undo.undoRedoPerformed += UndoRedoPerformed;
-	}
+    public TileMap tileMap
+    {
+        get { return target as TileMap; }
+    }
 
-	void OnDisable () {
-		Undo.undoRedoPerformed -= UndoRedoPerformed;
-	}
+    void OnEnable()
+    {
+        Undo.undoRedoPerformed += UndoRedoPerformed;
+    }
 
-	void UndoRedoPerformed () { 
-		tileMap.UpdateMesh();
-	}
+    void OnDisable()
+    {
+        Undo.undoRedoPerformed -= UndoRedoPerformed;
+    }
 
-	private Rect textureRect;
-	public override void OnInspectorGUI() {
-		Event e = Event.current;
-		
-		EditorGUI.BeginChangeCheck();
-		int mapWidth = EditorGUILayout.IntField("Map Width", tileMap.width);
-		int mapHeight = EditorGUILayout.IntField("Map Height", tileMap.height);
-		if (EditorGUI.EndChangeCheck()) {
-			Undo.RecordObject(target, "resize map");
-			tileMap.ResizeTileArray(mapWidth, mapHeight);
-			tileMap.UpdateMesh();
-		}
+    void UndoRedoPerformed()
+    {
+        tileMap.UpdateMesh();
+    }
 
-		base.OnInspectorGUI();
+    private Rect textureRect;
+    public override void OnInspectorGUI()
+    {
+        Event e = Event.current;
 
-		if (tileMap.texture == null) return;
+        EditorGUI.BeginChangeCheck();
+        int mapWidth = EditorGUILayout.IntField("Map Width", tileMap.width);
+        int mapHeight = EditorGUILayout.IntField("Map Height", tileMap.height);
+        if (EditorGUI.EndChangeCheck())
+        {
+            Undo.RecordObject(target, "resize map");
+            tileMap.ResizeTileArray(mapWidth, mapHeight);
+            tileMap.UpdateMesh();
+        }
 
-		float w = Screen.width - 40;
-		float h = w * tileMap.texture.height / tileMap.texture.width;
-		if (w > tileMap.texture.width) {
-			w = tileMap.texture.width;
-			h = tileMap.texture.height;
-		}
-		textureRect = GUILayoutUtility.GetRect(w, h);
-		textureRect.width = textureRect.height * tileMap.texture.width / tileMap.texture.height;
-		GUI.DrawTexture(textureRect, tileMap.texture);
+        base.OnInspectorGUI();
 
-		Vector2[] uvs = tileMap.GetUVs(tile);
-		Vector3[] verts = System.Array.ConvertAll(uvs, (u) => {
-			Vector3 v = (Vector3)(textureRect.position);
-			v.x += textureRect.width * u.x;
-			v.y += textureRect.height * (1 - u.y);
-			return v;
-		});
+        if (tileMap.texture == null) return;
 
-		Handles.DrawSolidRectangleWithOutline(verts, Color.clear, Color.white);
-		HandleUtility.Repaint();
+        float w = Screen.width - 40;
+        float h = w * tileMap.texture.height / tileMap.texture.width;
+        if (w > tileMap.texture.width)
+        {
+            w = tileMap.texture.width;
+            h = tileMap.texture.height;
+        }
+        textureRect = GUILayoutUtility.GetRect(w, h);
+        textureRect.width = textureRect.height * tileMap.texture.width / tileMap.texture.height;
+        GUI.DrawTexture(textureRect, tileMap.texture);
 
-		if (e.type == EventType.MouseDown) {
-			if (textureRect.Contains(e.mousePosition)) {
-				Vector2 pos = e.mousePosition - textureRect.position;
-				pos.x *= tileMap.columns / textureRect.width;
-				pos.y *= tileMap.rows / textureRect.height;
-				pos.y = tileMap.rows - pos.y;
-				tile = (int)pos.x + (int)pos.y * tileMap.columns;
-			}
-		}
-	}
+        Vector2[] uvs = tileMap.GetUVs(tile);
+        Vector3[] verts = System.Array.ConvertAll(uvs, (u) =>
+        {
+            Vector3 v = (Vector3)(textureRect.position);
+            v.x += textureRect.width * u.x;
+            v.y += textureRect.height * (1 - u.y);
+            return v;
+        });
 
-	void OnSceneGUI() {
-		Event e = Event.current;
+        Handles.DrawSolidRectangleWithOutline(verts, Color.clear, Color.white);
+        HandleUtility.Repaint();
 
-		if (!e.isMouse) return;
+        if (e.type == EventType.MouseDown)
+        {
+            if (textureRect.Contains(e.mousePosition))
+            {
+                Vector2 pos = e.mousePosition - textureRect.position;
+                pos.x *= tileMap.columns / textureRect.width;
+                pos.y *= tileMap.rows / textureRect.height;
+                pos.y = tileMap.rows - pos.y;
+                tile = (int)pos.x + (int)pos.y * tileMap.columns;
+            }
+        }
+    }
 
-		Vector3 pos = MouseToWorld();
-		int x = (int)pos.x;
-		int y = (int)pos.y;
-		bool paintEvent = (e.button < 2) && (e.type == EventType.MouseDown || e.type == EventType.MouseDrag);
-		if (paintEvent && x >= 0 && x < tileMap.width && y >= 0 && y < tileMap.height) {
-			if (e.type == EventType.MouseDown) GUIUtility.hotControl = GUIUtility.GetControlID(FocusType.Passive);
-			Undo.RecordObject(target, "resize map");
-			if (e.button == 0) {
-				tileMap.SetTile(x, y, tile + 1);
-			}
-			else if (e.button == 1) {
-				tileMap.SetTile(x, y, 0);
-			}
-			e.Use();
-		}
-		
-		if (e.type == EventType.MouseUp) GUIUtility.hotControl = 0;
-	}
+    void OnSceneGUI()
+    {
+        Event e = Event.current;
 
-	Vector3 MouseToWorld () {
-		Event e = Event.current;
-		Ray ray = HandleUtility.GUIPointToWorldRay(e.mousePosition);
-		Plane plane = new Plane(Vector3.forward, tileMap.transform.position);
-		float dist = 0;
-		if (plane.Raycast(ray, out dist)) {
-			return ray.GetPoint(dist) - tileMap.transform.position;
-		}
-		return Vector3.zero;
-	}
+        if (!e.isMouse) return;
 
-	public override bool HasPreviewGUI() {
-		return true;
-	}
+        Vector3 pos = MouseToWorld();
+        int x = (int)pos.x;
+        int y = (int)pos.y;
+        bool paintEvent = (e.button < 2) && (e.type == EventType.MouseDown || e.type == EventType.MouseDrag);
+        if (paintEvent && x >= 0 && x < tileMap.width && y >= 0 && y < tileMap.height)
+        {
+            if (e.type == EventType.MouseDown) GUIUtility.hotControl = GUIUtility.GetControlID(FocusType.Passive);
+            Undo.RecordObject(target, "resize map");
+            if (e.button == 0)
+            {
+                tileMap.SetTile(x, y, tile + 1);
+            }
+            else if (e.button == 1)
+            {
+                tileMap.SetTile(x, y, 0);
+            }
+            e.Use();
+        }
 
-	public override GUIContent GetPreviewTitle () {
-		return new GUIContent("Collision Editor");
-	}
+        if (e.type == EventType.MouseUp) GUIUtility.hotControl = 0;
+    }
 
-	public override void OnPreviewGUI(Rect r, GUIStyle background) {
-		Vector2[] uvs = tileMap.GetUVs(tile);
-		Vector2 size = uvs[2] - uvs[0];
-		Rect uvRect = new Rect(uvs[0].x, uvs[0].y, size.x, size.y);
-		float width = tileMap.texture.width * size.x;
-		float height = tileMap.texture.height * size.y;
-		r.x += (Screen.width - width) * 0.5f;
-		r.y += (r.height - height) * 0.5f;
-		r.width = width;
-		r.height = height;
-		GUI.DrawTextureWithTexCoords(r, tileMap.texture, uvRect, true);
+    Vector3 MouseToWorld()
+    {
+        Event e = Event.current;
+        Ray ray = HandleUtility.GUIPointToWorldRay(e.mousePosition);
+        Plane plane = new Plane(Vector3.forward, tileMap.transform.position);
+        float dist = 0;
+        if (plane.Raycast(ray, out dist))
+        {
+            return ray.GetPoint(dist) - tileMap.transform.position;
+        }
+        return Vector3.zero;
+    }
+
+    public override bool HasPreviewGUI()
+    {
+        return true;
+    }
+
+    public override GUIContent GetPreviewTitle()
+    {
+        return new GUIContent("Collision Editor");
+    }
+
+    public override void OnPreviewGUI(Rect r, GUIStyle background)
+    {
+        Vector2[] uvs = tileMap.GetUVs(tile);
+        Vector2 size = uvs[2] - uvs[0];
+        Rect uvRect = new Rect(uvs[0].x, uvs[0].y, size.x, size.y);
+        float width = tileMap.texture.width * size.x;
+        float height = tileMap.texture.height * size.y;
+        r.x += (Screen.width - width) * 0.5f;
+        r.y += (r.height - height) * 0.5f;
+        r.width = width;
+        r.height = height;
+        GUI.DrawTextureWithTexCoords(r, tileMap.texture, uvRect, true);
 
         List<Vector2> points = tileMap.GetPolygon(tile);
         if (points == null) return;
@@ -143,5 +163,6 @@ public class TileMapEditor : Editor {
 
         Handles.DrawPolyLine(path);
         HandleUtility.Repaint();
-	}
+    }
+
 }
